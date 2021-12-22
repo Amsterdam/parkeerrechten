dfc7b2443cb4ec3ebf01fba8c2e3c1499a20f8e2
Jeffrey Klardie <j.klardie@amsterdam.nl>
2021-12-22 16:14:54 +0100

    Implemented simple exporter

---
+++ src/exporter.py
@@ -0,0 +1,31 @@
+from datetime import timedelta
+
+import settings
+from database import LocalDatabase
+from object_store import ObjectStore
+
+
+class Exporter:
+    def __init__(self):
+        super().__init__()
+        self.local_db = LocalDatabase()
+        self.object_store = ObjectStore()
+
+    def export_orphans(self):
+        batch_names = [settings.NPR_NULL_VALUE]
+        self._run_export(batch_names)
+
+    def export_range(self, start_date, num_days_to_export):
+        batch_names = self.get_batch_names_for_export(start_date, num_days_to_export)
+        self._run_export(batch_names)
+
+    def _run_export(self, batch_names):
+        for batch_name in batch_names:
+            filename = f"{batch_name}_NPR_BACKUP.csv"
+            self.local_db.export_batch_to_csv(filename, batch_name)
+
+    def get_batch_names_for_export(self, start_date, num_days_to_export):
+        batch_names = [
+            (start_date + timedelta(days=days)).strftime("%Y%m%d") for days in range(num_days_to_export)
+        ]
+        return batch_names
