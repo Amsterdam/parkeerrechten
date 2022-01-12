f7a49f377a704e25a48f77ab89c2f6a8a19fd7f1
Jeffrey Klardie <j.klardie@amsterdam.nl>
2022-01-12 13:14:17 +0100

    Feature/importer (#3)
    * Implemented importer
    
    * Importer testcases
    
    * PR feedback

---
+++ src/importer.py
@@ -0,0 +1,86 @@
+import datetime
+import logging
+import sys
+import time
+
+import settings
+from database import NPRDatabase, LocalDatabase
+from object_store import ObjectStore
+from util import filter_batch_names
+
+
+class Importer:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.WARNING,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+    def __init__(self):
+        super().__init__()
+        self.local_db = LocalDatabase()
+        self.npr_db = NPRDatabase()
+        self.object_store = ObjectStore()
+
+    def import_orphans(self):
+        batch_names = [settings.NPR_NULL_VALUE]
+        self._run_import(batch_names)
+
+    def import_range(self, start_date, end_date):
+        today = datetime.date.today()
+        if end_date >= today:
+            # ensure we never import today as its data is still incomplete
+            end_date = today - datetime.timedelta(days=1)
+
+        self.logger.info("Determining batch names for import...")
+        batch_names = self.get_batch_names_for_download(start_date, end_date)
+        self.logger.info(f"{len(batch_names)} batches found to import")
+        self._run_import(batch_names)
+
+    def import_last_x_days(self, num_days):
+        today = datetime.date.today()
+        start_date = today - datetime.timedelta(days=num_days)
+        return self.import_range(start_date, today)
+
+    def _run_import(self, batch_names):
+        if not batch_names:
+            self.logger.info("Nothing to import")
+            return
+
+        for batch_name in batch_names:
+            self.backup_batch(batch_name)
+
+    def get_batch_names_for_download(self, start_date, end_date):
+        # We want batches that are requested and not yet backed up
+        # (these are the set of candidates to back up).
+
+        batch_names_in_local_db = self.local_db.get_existing_batch_names(
+            require_table=False
+        )
+        batch_names_in_obj_store = self.object_store.get_existing_batch_names()
+        backed_up = batch_names_in_obj_store + batch_names_in_local_db
+
+        batch_names = self.npr_db.get_existing_batch_names()
+        batch_names = filter_batch_names(start_date, end_date, batch_names)
+
+        batch_names = list(set(batch_names) - set(backed_up))
+        batch_names.sort()
+        return batch_names
+
+    def backup_batch(self, batch_name):
+        """
+        Retrieve records from NPR, store them in local database in batches.
+        """
+        self.logger.info(f"Backing up batch {batch_name}. Getting iterator...")
+        npr_backup_iterator = self.npr_db.get_backup_iterator(
+            batch_name, batch_size=settings.BATCH_SIZE
+        )
+
+        self.logger.info("Starting local import")
+        start = time.perf_counter()
+        self.local_db.backup_iterator(npr_backup_iterator)
+        end = time.perf_counter()
+        self.logger.info(f">> Done. Processing iterator took {end - start:0.2f}s")
---
+++ tests/test_importer.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+import pytest
+
+from importer import Importer
+from models import NPRTable
+
+
+class TestImporter:
+    @pytest.mark.parametrize(
+        "start_date_str,end_date_str,num_expected",
+        [
+            ("20170731", "20170731", 0),     # first day is 01/08 so no import
+            ("20170801", "20170801", 8),     # 8 on first day
+            ("20170825", "20170825", 2),     # 2 on last day
+            ("20170826", "20170826", 0),     # last day is 25/08 so no import
+            ("20170731", "20170826", 84),    # 84 in time window
+        ],
+    )
+    def test_import_range(self, local_db, start_date_str, end_date_str, num_expected):
+        importer = Importer()
+        start_date = datetime.strptime(start_date_str, "%Y%m%d").date()
+        end_date = datetime.strptime(end_date_str, "%Y%m%d").date()
+        importer.import_range(start_date, end_date)
+
+        with local_db.session() as session:
+            num = session.query(NPRTable).count()
+            assert num == num_expected
+
+    def test_import_orphans(self, local_db):
+        # assert that we import all 16 orphans (batch name = "Leeg") in de test csv
+        importer = Importer()
+        importer.import_orphans()
+
+        with local_db.session() as session:
+            num = session.query(NPRTable).count()
+            assert num == 16
